8fface9ecd22f97cf20417e9189fa61e21d169e3
ZamDimon <zamdmytro@gmail.com>
2024-04-05 14:38:04 +0300

    :recycle: make bn256 types public

--- src/gadgets/curves/bn256/mod.rs
+++ src/gadgets/curves/bn256/mod.rs
@@ -20,34 +20,26 @@ pub mod ec_pairing;
 
 // --- Base and scalar field params for BN256 curve ---
 /// Params of BN256 base field
-type BN256BaseNNFieldParams = NonNativeFieldOverU16Params<BN256Fq, 17>;
+pub type BN256BaseNNFieldParams = NonNativeFieldOverU16Params<BN256Fq, 17>;
 /// Params of BN256 scalar field
-type BN256ScalarNNFieldParams = NonNativeFieldOverU16Params<BN256Fr, 17>;
+pub type BN256ScalarNNFieldParams = NonNativeFieldOverU16Params<BN256Fr, 17>;
 /// Non-native field over u16 for BN256 base field
-type BN256BaseNNField<F> = NonNativeFieldOverU16<F, BN256Fq, 17>;
+pub type BN256BaseNNField<F> = NonNativeFieldOverU16<F, BN256Fq, 17>;
 /// Non-native field over u16 for BN256 scalar field
-type BN256ScalarNNField<F> = NonNativeFieldOverU16<F, BN256Fr, 17>;
+pub type BN256ScalarNNField<F> = NonNativeFieldOverU16<F, BN256Fr, 17>;
 
 // P.S. we used 17 bits since 17 bits * 16 bits in u16 = 272 bits > 254 bits
 // used in BN254 (so we have some extra space to deal with)
 
 // --- Field extensions for BN256 curve ---
 /// Non-native field extension Fq2 for BN256 curve
-type BN256Fq2NNField<F> = Fq2<F, BN256Fq, BN256BaseNNField<F>, BN256Extension2Params>;
+pub type BN256Fq2NNField<F> = Fq2<F, BN256Fq, BN256BaseNNField<F>, BN256Extension2Params>;
 /// Non-native field extension Fq12 for BN256 curve
-type BN256Fq12NNField<F> = Fq12<F, BN256Fq, BN256BaseNNField<F>, BN256Extension12Params>;
+pub type BN256Fq12NNField<F> = Fq12<F, BN256Fq, BN256BaseNNField<F>, BN256Extension12Params>;
 
 // --- SW Projective points for BN256 curves: regular and twisted ---
 /// SW Projective point for BN256 curve over non-extended base field
-type BN256SWProjectivePoint<F> = SWProjectivePoint<F, BN256Affine, BN256BaseNNField<F>>;
+pub type BN256SWProjectivePoint<F> = SWProjectivePoint<F, BN256Affine, BN256BaseNNField<F>>;
 /// SW Projective point for twisted BN256 curve over extended base field `Fp2`
-type BN256SWProjectivePointTwisted<F> =
+pub type BN256SWProjectivePointTwisted<F> =
     ExtendedSWProjectivePoint<F, BN256Fq, BN256AffineTwisted, BN256Fq2NNField<F>>;
-
-fn bn256_base_field_params() -> BN256BaseNNFieldParams {
-    NonNativeFieldOverU16Params::create()
-}
-
-fn bn256_scalar_field_params() -> BN256ScalarNNFieldParams {
-    NonNativeFieldOverU16Params::create()
-}
