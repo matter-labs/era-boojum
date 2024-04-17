7614c6aba567561c1439abdfcdd77ec07ba740f9
Nikolay Kurtov <nkurtov@ulvetanna.io>
2024-04-17 16:51:01 +0200

    nightly

--- sha256_bench_non_recursive.sh
+++ sha256_bench_non_recursive.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-RUSTFLAGS='-C target-cpu=native' cargo +nightly test --release -- --ignored --nocapture run_sha256_prover_non_recursive
+RUSTFLAGS='-C target-cpu=native -C target-feature=+avx2' cargo +nightly test --release -- --ignored --nocapture run_sha256_prover_non_recursive
